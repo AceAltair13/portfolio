c97e2a740ab1d8c91625ecccd59232b4e064f6d1
Tirth <55307429+AceAltair13@users.noreply.github.com>
2023-12-29 03:51:19 -0800

    fixed apos issue

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -14,8 +14,10 @@ export default function page() {
           </div>
           <div className="col-span-2 px-12">
             <FaQuoteLeft className="text-3xl text-slate-200" />
-            <p className="text-lg text-slate-300 py-4 text-justify">Hello! My name is Tirth Thoria and I am currently pursuing Masters in Computer Science at California State University, Long Beach. I am a former Advanced App Engineering Associate at Accenture.<br /><br />
-              I'm a highly motivated software developer with a track record of delivering exceptional results in collaborative team environments. Known for strong leadership and organizational abilities, I've successfully managed projects and mentored others. Committed to making a positive impact through a relentless work ethic, I'm a passionate coder, avid gamer, and dedicated volunteer, driven by a desire to innovate and contribute to meaningful projects.</p>
+            <p className="text-lg text-slate-300 py-4 text-justify">
+              {"Hello! My name is Tirth Thoria and I am currently pursuing Masters in Computer Science at California State University, Long Beach. I am a former Advanced App Engineering Associate at Accenture."}
+              <br /><br />
+              {" I'm a highly motivated software developer with a track record of delivering exceptional results in collaborative team environments. Known for strong leadership and organizational abilities, I've successfully managed projects and mentored others. Committed to making a positive impact through a relentless work ethic, I'm a passionate coder, avid gamer, and dedicated volunteer, driven by a desire to innovate and contribute to meaningful projects."}</p>
             <FaQuoteRight className="text-3xl text-slate-200 ml-auto" />
           </div>
         </div>
