73807c0438f5e62a43860704b6b9153c1f934d19
Tirth <55307429+AceAltair13@users.noreply.github.com>
2023-12-28 23:55:43 -0800

    added projects

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
     return (
         <div className='min-h-svh grid justify-around items-center grid-cols-2'>
-            <div className='col-span-1 bg-gray-800 bg-opacity-50 p-8 rounded-3xl'>
+            <div className='col-span-1 bg-gray-800 bg-opacity-85 p-8 rounded-3xl'>
                 <h1 className='text-6xl font-extrabold text-slate-50 mb-4'>Hi, I am <span className="text-amber-500">Tirth</span>🙋🏻‍♂️</h1>
                 <h2 className='text-2xl text-slate-300 mb-8'>A Passionate Programmer, Gamer and Student</h2>
                 <p className='text-lg text-slate-400 mb-12'>
--- app/data/projects.js
+++ app/data/projects.js
@@ -1,9 +1,28 @@
 import { techSkills } from "./skills";
+import PortfolioImage from "../../public/portfolio.png";
 import SchedulexImage from "../../public/schedulex.png";
 import ExaminatorImage from "../../public/examinator.png";
 import OpenRestaurantImage from "../../public/openrestaurant.png";
+import FoodzillaImage from "../../public/foodzilla.png";
+import EventManagementSystem from "../../public/eventmanagementsystem.png";
+import MuiStickyNotesImage from "../../public/muistickynotes.png";
+import SmartSecureLockImage from "../../public/smartsecurelock.png";
 
 export const projects = [
+  {
+    title: "Portfolio",
+    link: "https://github.com/AceAltair13/portfolio",
+    image: PortfolioImage,
+    description: "My portfolio website built using Next.js 14.0 and Tailwind CSS.",
+    skills: [
+      techSkills.nextjs,
+      techSkills.reactjs,
+      techSkills.tailwindcss,
+      techSkills.javascript,
+      techSkills.html,
+      techSkills.css,
+    ]
+  },
   {
     title: "Schedulex",
     link: "https://github.com/AceAltair13/schedulex",
@@ -14,7 +33,7 @@ export const projects = [
   },
   {
     title: "Examinator",
-    link: "",
+    link: "https://github.com/AceAltair13/proctor-website",
     image: ExaminatorImage,
     description:
       "Examinator is an online examination system with automatic realtime proctoring capabilities.",
@@ -26,19 +45,72 @@ export const projects = [
       techSkills.expressjs,
       techSkills.materialui,
       techSkills.tensorflowjs,
-      techSkills.restapi
+      techSkills.restapi,
     ],
   },
   {
     title: "OpenRestaurant",
-    link: "",
+    link: "https://github.com/AceAltair13/OpenRestaurant",
     image: OpenRestaurantImage,
-    description: "OpenRestaurant is a food order placing app which displays menus of nearby restaurants and food trucks.",
+    description:
+      "OpenRestaurant is a food order placing app which displays menus of nearby restaurants and food trucks.",
     skills: [
       techSkills.kotlin,
       techSkills.firebase,
       techSkills.sqlite,
       techSkills.androidstudio,
-    ]
-  }
+    ],
+  },
+  {
+    title: "Foodzilla",
+    link: "https://github.com/AceAltair13/Foodzilla-A-Recipe-IO",
+    image: FoodzillaImage,
+    description:
+      "Foodzilla is a food recipe finding website. You can even use it to share your own recipes.",
+    skills: [
+      techSkills.html,
+      techSkills.css,
+      techSkills.javascript,
+      techSkills.bootstrap,
+      techSkills.php,
+      techSkills.django,
+      techSkills.mysql,
+    ],
+  },
+  {
+    title: "Event Management System",
+    link: "https://github.com/AceAltair13/Event-Management-System--Flask",
+    image: EventManagementSystem,
+    description:
+      "Alpha Events is an Event Management System used for booking and managing birthdays, anniversaries and other events.",
+    skills: [
+      techSkills.python,
+      techSkills.flask,
+      techSkills.html,
+      techSkills.bootstrap,
+      techSkills.mariadb,
+    ],
+  },
+  {
+    title: "MUI Sticky Notes",
+    link: "https://github.com/AceAltair13/material-ui-sticky-notes",
+    image: MuiStickyNotesImage,
+    description:
+      "MUI Sticky Notes is a React App utilizing Material UI used to create and manage sticky notes.",
+    skills: [techSkills.reactjs, techSkills.materialui],
+  },
+  {
+    title: "Smart & Secure Lock",
+    link: "https://github.com/krupalvora/Smart-SecureLock",
+    image: SmartSecureLockImage,
+    description:
+      "Smart & Secure Lock is an IoT project utilizing a Flask backend to send and receive requests from an ESP32 board.",
+    skills: [
+      techSkills.flask,
+      techSkills.python,
+      techSkills.bootstrap,
+      techSkills.firebase,
+      techSkills.heroku,
+    ],
+  },
 ];
